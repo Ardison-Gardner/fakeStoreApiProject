88e188e00294b2ee0ef20f1c8466542a257db34a
Gregory Ardison-Gardner <greg.ardison.gardner@gmail.com>
2022-10-22 21:17:17 +0000

    Update AllProducts.js and Home.js; moved useFetch to Home and pass products as props to AllProducts, which now renders the list.

--- src/components/AllProducts.js
+++ src/components/AllProducts.js
@@ -1,27 +1,14 @@
-import { useState, useEffect } from "react";
-
-const AllProducts = () => {
-  const [products, setProducts] = useState([]);
-  /* const products = useFetch("https://fakestoreapi.com/products");
-  console.log(products);*/
-
-  useEffect(() => {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Could not fetch data for this resource!");
-        }
-        return res.json();
-      })
-      .then((products) => {
-        setProducts(products);
-        console.log("Products: ", products);
-      });
-  }, []);
-
+const AllProducts = ({ products, title }) => {
+  console.log("AllProducts: ", products);
   return (
-    <div className="products-list">
-      <h2>All Products</h2>
+    <div className="product-list">
+      <h2>{title}</h2>
+      {products.map((product) => (
+        <div className="product-preview" key={product.id}>
+          <h2>{product.title}</h2>
+          <p>{product.price}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,26 @@
+import { useState, useEffect } from "react";
 import AllProducts from "./AllProducts";
 
 const Home = () => {
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Could not fetch data for this resource!");
+        }
+        return res.json();
+      })
+      .then((products) => {
+        setProducts(products);
+      });
+  }, []);
+
   return (
     <div className="home">
       <h1>HOME</h1>
-      <AllProducts />
+      {products && <AllProducts products={products} title="All Products" />}
     </div>
   );
 };
