c6e7d79f647410d7ccd4cb0f8e411e6d95de3180
Gregory Ardison-Gardner <greg.ardison.gardner@gmail.com>
2022-10-22 00:39:40 +0000

    Created AllProducts component, which will render a list of all available products from the api.

---
+++ src/components/AllProducts.js
@@ -0,0 +1,9 @@
+const AllProducts = () => {
+  return (
+    <div className="all-products">
+      <h2>All Products</h2>
+    </div>
+  );
+};
+
+export default AllProducts;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,10 @@
+import AllProducts from "./AllProducts";
+
 const Home = () => {
   return (
     <div className="home">
       <h1>HOME</h1>
+      <AllProducts />
     </div>
   );
 };
